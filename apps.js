33d44f85ccaddfe5a4adc683dfb1d5fe832ad629
Gabe075 <118068728+Gabe075@users.noreply.github.com>
2025-12-04 15:57:12 -0300

    Update app version and download link in apps.js

--- apps.js
+++ apps.js
@@ -14,8 +14,8 @@ window.APPS = [
       "Compatível com celulares Android recentes.",
     tag: "apps",
     size: "27 MB",
-    version: "v3.4 Premium Mod",
-    download: "files/dramabox.apk", 
+    version: "v4.8.1 Premium Mod",
+    download: "https://download2348.mediafire.com/nnb52djqpj0gn2NXrSARe_29j3_iJcjSB1HOXIVgytMJg_Sg1R2SzRr1meKZVOlcPzpYy_SHS6HTy90Fi5Y9fy8i3ouoNHSN8Zrff-qb_6LQM5XCO7Rp_K_YFu0gy2Ol5LcrmriL1uZ2OuDJNIbuQXcUjKJ_gLkgC6_1k45JGay5gQ/1r2883g46a6m3oa/DramaBox_Mod+-+v4.8.1_Tekmods.com.apk", 
     icon: "https://raw.githubusercontent.com/Gabe075/Imagem/refs/heads/main/unnamed.webp",
     screenshots: [
       "https://raw.githubusercontent.com/Gabe075/Imagem/refs/heads/main/unnamed%20(3).webp",
@@ -23,7 +23,7 @@ window.APPS = [
       "https://raw.githubusercontent.com/Gabe075/Imagem/refs/heads/main/unnamed%20(1).webp"
     ],
     changelog:
-      "v3.4 Mod — Recursos premium liberados, remoção de anúncios, player otimizado, " +
+      "v4.8.1 Mod — Recursos premium liberados, remoção de anúncios, player otimizado, " +
       "melhorias de performance e correções de estabilidade."
   }
 ];
